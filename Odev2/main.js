fb70021c5628e29e73606cdd222f3662f32817bf
aenesyvz <20060403@stu.omu.edu.tr>
2023-05-13 20:46:41 +0300

    isim değişikliği

--- Odev2/main.js
+++ Odev2/main.js
@@ -1,144 +1,25 @@
-const previousProcess = document.querySelector('.previous__process')
-const currentProcess = document.querySelector('.current__process')
-
-const resultBtn = document.querySelector('.result__btn')
-const clearBtn = document.querySelector('.clear__btn')
-const backBtn = document.querySelector('.back__btn')
-
-const operatorBtn = document.querySelectorAll('.operator__btn')
-const numberBtn = document.querySelectorAll('.data-number')
-
-
-class Calculator {
-    constructor(previousProcess, currentProcess) {
-        this.previousProcess = previousProcess;
-        this.currentProcess = currentProcess;
-        this.clear();
-    }
-
-    selectOperator(operator) {
-        if (this.currentOperator === ''){ 
-            return;
-        }
-
-        if (this.previousOperator !== '') {
-            this.run();
-        }
-        
-        this.previousOperator = this.currentOperator;
-        this.currentOperator = '';
-        this.operator = operator;
-    }
-
-    add(number) {
-        if (number === '.' && this.currentOperator.includes('.')) 
-        {
-            return;
-        }  
-        this.currentOperator = this.currentOperator.toString() + number.toString();
-    }
-
-    delete() {
-        this.currentOperator = this.currentOperator.toString().slice(0, -1);
-    }
-
-   
-   
-    run() {
-        let result;
-        const prevValue = parseFloat(this.previousOperator)
-        const currentValue = parseFloat(this.currentOperator)
-        if (isNaN(prevValue) || isNaN(currentValue)) {
-            return
-        }
-        else if(this.operator === "%"){
-            result = prevValue % currentValue;
-        }
-        else if(this.operator === "+"){
-            result = prevValue + currentValue;
-        }else if(this.operator === "x"){
-            result = prevValue * currentValue;
-        }
-        else if(this.operator === "-"){
-            result = prevValue - currentValue;
-        }else if(this.operator === "/"){
-            result = prevValue / currentValue;
-        }else if(this.operator === "^"){
-            result = Math.pow(prevValue,currentValue);
-        }
-       
-        this.currentOperator = result
-        this.operator = undefined
-        this.previousOperator = ''  
-    }
-
-    clear() {
-        this.currentOperator = ''
-        this.previousOperator = ''
-        this.operator = undefined
-    }
-
-    getNumber(number) {
-        const integerNumber = parseFloat(number.toString().split('.')[0])
-        const decimalNumber = number.toString().split('.')[1]
-        let showNumber;
-        if (isNaN(integerNumber)) {
-            showNumber = '';
-        } else {
-            showNumber = integerNumber.toLocaleString('tr', { maximumFractionDigits: 0 })
-        }
-        if (decimalNumber != null) {
-            return `${showNumber}.${decimalNumber}`
-        } else {
-            return showNumber;
-        }
-    }
-
-    show() {
-        this.currentProcess.innerText =
-            this.getNumber(this.currentOperator)
-        if (this.operator != null) {
-            this.previousProcess.innerText =
-                `${this.getNumber(this.previousOperator)} ${this.operator}`
-        } else {
-            this.previousProcess.innerText = ''
-        }
-    }
-}
-
-
-
-
-const calculator = new Calculator(previousProcess, currentProcess)
-
-resultBtn.addEventListener('click', () => {
-    calculator.run();
-    calculator.show();
-});
-
-clearBtn.addEventListener('click', () => {
-    calculator.clear();
-    calculator.show();
-});
-
-
-backBtn.addEventListener('click', () => {
-    calculator.delete();
-    calculator.show();
-});
-
-operatorBtn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        calculator.selectOperator(btn.innerText);
-        calculator.show();
-    })
-});
-
-numberBtn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        calculator.add(btn.innerText);
-        calculator.show();
-    })
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2800,
+    delay:0
+    // reset: true,
 })
 
+sr.reveal(`.title,.section__title,.skill-l`,{
+    origin:"left",
+    interval:100
+})
 
+sr.reveal(`.sub__title,.section__border,.skill-r`,{
+    origin:"right",
+    interval:100
+})
+
+sr.reveal(`.about__img,.info,.indexBtn`,{
+    origin:"top",
+    interval: 100,
+})
+
+sr.reveal(`.about__data,.slider`,{
+    origin:"bottom"
+})
--- Odev4/main.js
+++ Odev4/main.js
@@ -1,25 +1,142 @@
-const sr = ScrollReveal({
-    distance: '60px',
-    duration: 2800,
-    delay:0
-    // reset: true,
+const previousExpression = document.querySelector('.previous__expression')
+const currentExpression = document.querySelector('.current__expression')
+
+const resultBtn = document.querySelector('.result__btn')
+const clearBtn = document.querySelector('.clear__btn')
+const backBtn = document.querySelector('.back__btn')
+
+const operatorBtn = document.querySelectorAll('.operator__btn')
+const numberBtn = document.querySelectorAll('.data-number')
+
+
+class Calculator {
+    constructor(previousExpression, currentExpression) {
+        this.previousExpression = previousExpression;
+        this.currentExpression = currentExpression;
+        this.clear();
+    }
+
+    selectOperator(operator) {
+        if (this.currentProcess === ''){ 
+            return;
+        }
+
+        if (this.previousProcess !== '') {
+            this.run();
+        }
+        
+        this.previousProcess = this.currentProcess;
+        this.currentProcess = '';
+        this.operator = operator;
+    }
+
+    add(number) {
+        if (number === '.' && this.currentProcess.includes('.')) 
+        {
+            return;
+        }  
+        this.currentProcess = this.currentProcess.toString() + number.toString();
+    }
+
+    delete() {
+        this.currentProcess = this.currentProcess.toString().slice(0, -1);
+    }
+
+   
+   
+    run() {
+        let result;
+        const firstValue = parseFloat(this.previousProcess)
+        const secondValue = parseFloat(this.currentProcess)
+        if (isNaN(firstValue) || isNaN(secondValue)) {
+            return
+        }
+        else if(this.operator === "%"){
+            result = firstValue % secondValue;
+        }
+        else if(this.operator === "+"){
+            result = firstValue + secondValue;
+        }else if(this.operator === "x"){
+            result = firstValue * secondValue;
+        }
+        else if(this.operator === "-"){
+            result = firstValue - secondValue;
+        }else if(this.operator === "/"){
+            result = firstValue / secondValue;
+        }else if(this.operator === "^"){
+            result = Math.pow(firstValue,secondValue);
+        }
+       
+        this.currentProcess = result
+        this.operator = undefined
+        this.previousProcess = ''  
+    }
+
+    clear() {
+        this.currentProcess = ''
+        this.previousProcess = ''
+        this.operator = undefined
+    }
+
+    getNumber(number) {
+        const integerNumber = parseFloat(number.toString().split('.')[0])
+        const decimalNumber = number.toString().split('.')[1]
+        let showNumber;
+        if (isNaN(integerNumber)) {
+            showNumber = '';
+        } else {
+            showNumber = integerNumber.toLocaleString('tr', { maximumFractionDigits: 0 })
+        }
+        if (decimalNumber != null) {
+            return `${showNumber}.${decimalNumber}`
+        } else {
+            return showNumber;
+        }
+    }
+
+    show() {
+        this.currentExpression.innerText = this.getNumber(this.currentProcess)
+        if (this.operator != null) {
+            this.previousExpression.innerText = `${this.getNumber(this.previousProcess)} ${this.operator}`
+        } else {
+            this.previousExpression.innerText = ''
+        }
+    }
+}
+
+
+
+
+const calculator = new Calculator(previousExpression, currentExpression)
+
+resultBtn.addEventListener('click', () => {
+    calculator.run();
+    calculator.show();
+});
+
+clearBtn.addEventListener('click', () => {
+    calculator.clear();
+    calculator.show();
+});
+
+
+backBtn.addEventListener('click', () => {
+    calculator.delete();
+    calculator.show();
+});
+
+operatorBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        calculator.selectOperator(btn.innerText);
+        calculator.show();
+    })
+});
+
+numberBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        calculator.add(btn.innerText);
+        calculator.show();
+    })
 })
 
-sr.reveal(`.title,.section__title,.skill-l`,{
-    origin:"left",
-    interval:100
-})
 
-sr.reveal(`.sub__title,.section__border,.skill-r`,{
-    origin:"right",
-    interval:100
-})
-
-sr.reveal(`.about__img,.info,.indexBtn`,{
-    origin:"top",
-    interval: 100,
-})
-
-sr.reveal(`.about__data,.slider`,{
-    origin:"bottom"
-})
